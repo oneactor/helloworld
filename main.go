2627fa32b680d7614a5947ab0711a51ae01a3fbd
oneactor <285761676@qq.com>
2016-06-06 16:48:31 +0800

    修改main.go

--- main.go
+++ main.go
@@ -1,34 +1,32 @@
 package main
 
 import (
-    "fmt"
-    // "html/template"
-    "log"
-    "net/http"
-    // "os"
-    // "strings"
+	"fmt"
+	// "html/template"
+	"log"
+	"net/http"
+	// "os"
+	// "strings"
 )
 
 func main() {
-    http.HandleFunc("/", index)
-    log.Println("Start listening...")
-    err := http.ListenAndServe(":80", nil)
-    if err != nil {
-        panic(err)
-    }
+	http.HandleFunc("/", index)
+	log.Println("Start listening...")
+	err := http.ListenAndServe(":80", nil)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func index(res http.ResponseWriter, req *http.Request) {
-    defer func() {
-        if e := recover(); e != nil {
-            log.Println(e)
-            res.WriteHeader(http.StatusInternalServerError)
-        }
-    }()
+	defer func() {
+		if e := recover(); e != nil {
+			log.Println(e)
+			res.WriteHeader(http.StatusInternalServerError)
+		}
+	}()
 
-    log.Println("Index home")
-    
-    res.Write([]byte(fmt.Sprintf("welcome ! 妮", 3)))
+	log.Println("Index home")
+
+	res.Write([]byte(fmt.Sprintf("welcome ! 妮")))
 }
-
-
